084d1ff34739262ddec771827633ff7d91c7a66f
Viacheslav V. Kovalevskyi <Viacheslav@b0noI.com>
2014-05-24 15:01:05 +0100

    rewriting read method

--- run_analysis.R
+++ run_analysis.R
@@ -4,15 +4,17 @@ Y_TRAIN_PATH <- "train/y_train.txt"
 X_TEST_PATH <- "test/X_test.txt"
 Y_TEST_PATH <- "test/y_test.txt"
 
-# This method is using for reading vectors from file
-read_vector <- function(path) {
-  return (scan(file=path, sep=" "))
-}
-
-# This method is reading 2 vectors from paths and creating one DataSet from it
-read_data_set <- function(path_to_left_vector, peth_to_right_vector) {
-  left_vector <- read_vector(path_to_left_vector)
-  right_vector <- read_vector(peth_to_right_vector)
-  return (cbind(left_vector, right_vector))
+# This method:
+#   1) reads data frame from path
+#   2) reads Activity labels from path to activity labels file
+#   3) add activity labels to main data frame
+read_data_frame <- function(path, path_to_activity_label) {
+  # Reading main DataFrame
+  data_set <- read.table(path, header=FALSE)
+  # Reading Activity Labels
+  activity_labels <- scan(path_to_activity_label)
+  # Adding Activity label to main DataFrame
+  data_set['Activity'] <- activity_labels
+  return(data_set)
 }
 
